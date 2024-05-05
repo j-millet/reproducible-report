8b02d195a44df52f498534b10b0e7a54261140c2
B0cz3k <113113404+B0cz3k@users.noreply.github.com>
2024-05-05 13:26:56 +0200

    wip

---
+++ load_data.R
@@ -0,0 +1,5 @@
+results2018 <- read.csv("./developer_surveys/survey_results_2018.csv")
+results_schema2018 <- read.csv("./developer_surveys/survey_results_schema_2018.csv")
+results2023 <- read.csv("./developer_surveys/survey_results_2023.csv")
+results_schema2023 <- read.csv("./developer_surveys/survey_results_schema_2023.csv")
+
